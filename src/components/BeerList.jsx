e878f360cf48e1f7f79375171dc087fc8a067bc3
fcaramez <francisco.caramez.duarte@gmail.com>
2022-08-19 15:05:14 +0100

    adds started code

---
+++ src/components/BeerList.jsx
@@ -0,0 +1,5 @@
+function BeerList() {
+  return <div></div>;
+}
+
+export default BeerList;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const chakraTheme = extendTheme({ config });
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={chakraTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
